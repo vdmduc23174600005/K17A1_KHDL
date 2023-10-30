245e44b0b89df4ed479491f2e5cdcad3cce8ff7e
ducvu17a1 <148187108+vdmduc23174600005@users.noreply.github.com>
2023-10-30 23:20:54 +0700

    Rename bt5_chuong4.py to bt5_c4.py

---
+++ bt5_c4.py
@@ -0,0 +1,4 @@
+so1 = -8
+so2 = 15
+BCNN = (so1 * so2) // (abs(so1 * so2))
+print(f"Bội chung nhỏ nhất của {so1} và {so2} là {abs(so1 * so2)}")
--- bt5_chuong4.py
+++
@@ -1,4 +0,0 @@
-so1 = -8
-so2 = 15
-BCNN = (so1 * so2) // (abs(so1 * so2))
-print(f"Bội chung nhỏ nhất của {so1} và {so2} là {abs(so1 * so2)}")
